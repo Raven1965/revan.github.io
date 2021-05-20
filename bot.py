e38c83c3af55ae08c1ff7ae650af5683e2098cf3
Raven1965 <74115718+Raven1965@users.noreply.github.com>
2021-05-20 10:51:33 +0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,77 @@
+import logging
+
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def callback_minute(context: telegram.ext.CallbackContext):
+    context.bot.send_message(chat_id='1783923206' ,
+    text='Dakikada bir mesaj gönderiyorum')
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    token = "1760229479:AAF6BhifaMJ8QQtzTlxp2XPM90-SV_gD68Y"
+    updater = Updater(token, use_context=True)
+    j = updater.job_queue
+
+    job_minute = j.run_repeating(callback_minute, interval=60, first=10)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    myuser_id = '1783923206'
+
+    updater.dispatcher.bot.send_message(chat_id=myuser_id, text='Ansızın mesaj gönderiyorum!')
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
